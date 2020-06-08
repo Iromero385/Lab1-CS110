b7bdcef18214c409ff8742e32b5216a09f8a9ac4
iromero385 <iromero385@berkeley.edu>
2020-06-07 17:49:56 -0700

    finish first programm

---
+++ Lab1/Lab1/lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+	int number1, number2; 
+	cout << "Tell me a number please: ";
+	cin >> number1; 
+	cout << "Tell me another number: ";
+	cin >> number2; 
+	cout << "your total is: " << number1 + number2;
+
+}
